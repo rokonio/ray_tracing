f92e7b4e4eb3140b352fb72d84eb8aba7c53c444
Rokonio <naelkanada@gmail.com>
2020-11-13 19:08:58 +0100

    First image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+
+	// Image
+
+	const IMG_WIDTH: u32 = 256;
+	const IMG_HEIGHT: u32 = 256;
+
+	// Render
+
+	println!("P3\n{} {}\n255", IMG_WIDTH, IMG_HEIGHT);
+
+	for j in (0..IMG_HEIGHT).rev() {
+		for i in 0..IMG_WIDTH {
+			let r = i as f64 / (IMG_WIDTH-1) as f64;
+			let g = j as f64 / (IMG_HEIGHT-1) as f64;
+			let b = 0.25;
+
+			let ir = (255.999 * r) as i32;
+			let ig = (255.999 * g) as i32;
+			let ib = (255.999 * b) as i32;
+
+			println!("{} {} {}", ir, ig, ib);
+		}
+	}
+
+	// Note: After building command >target\debug\ray_tracing.exe > output.ppm
 }
