56e5a5cbbf0f4f2ee6618c88564264c077190044
Rokonio <naelkanada@gmail.com>
2020-11-29 18:23:26 +0100

    Color the sphere with according to its normal vector

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ use vec3::*;
 use color::*;
 use ray::*;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
 	let oc = r.origin() - *center;
 	let a = dot(&r.direction(), &r.direction());
 	let b = 2. * dot(&oc, &r.direction());
 	let c = dot(&oc, &oc) - radius*radius;
 	let discriminant = b*b - 4.*a*c;
-	discriminant > 0.
+	if discriminant  < 0. {
+		return -1.;
+	} else {
+		return (-b - f64::sqrt(discriminant)) / (a * 2.);
+	}
 }
 
 fn ray_color(r: &Ray) -> Color {
-	if hit_sphere(&Point3::new(0., 0., -1.), 0.5, &r) {
-		return Color::new(1., 0., 0.);
+	let t = hit_sphere(&Point3::new(0., 0., -1.), 0.5, &r);
+	if t > 0. {
+		let n = unit_vector(&(r.at(t) - Vec3::new(0., 0., -1.)));
+		return 0.5*Color::new(n.x()+1., n.y()+1., n.z()+1.);
 	}
 	let unit_direction = unit_vector(&r.direction());
 	let t = 0.5*(unit_direction.y() + 1.);
@@ -59,7 +65,7 @@ fn main() {
 		for i in 0..IMG_WIDTH {
 			let u = i as f64 / (IMG_WIDTH-1) as f64;
 			let v = j as f64 / (IMG_HEIGHT-1) as f64;
-			
+
 			let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
 			let pixel_color = ray_color(&r);
 			write_color(&mut out, pixel_color);
