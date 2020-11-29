dbe13dd3a7c0ae41a87514092d1d9d437da827ea
Rokonio <naelkanada@gmail.com>
2020-11-29 18:08:09 +0100

    Add a simple red sphere

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,22 @@ use vec3::*;
 use color::*;
 use ray::*;
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+	let oc = r.origin() - *center;
+	let a = dot(&r.direction(), &r.direction());
+	let b = 2. * dot(&oc, &r.direction());
+	let c = dot(&oc, &oc) - radius*radius;
+	let discriminant = b*b - 4.*a*c;
+	discriminant > 0.
+}
+
 fn ray_color(r: &Ray) -> Color {
+	if hit_sphere(&Point3::new(0., 0., -1.), 0.5, &r) {
+		return Color::new(1., 0., 0.);
+	}
 	let unit_direction = unit_vector(&r.direction());
 	let t = 0.5*(unit_direction.y() + 1.);
-	(1. - t) * Color::new(1., 1., 1.) + t * Color::new(0.2, 0.5, 1.)
+	(1. - t) * Color::new(1., 1., 1.) + t * Color::new(0.5, 0.7, 1.)
 }
 
 fn main() {
@@ -45,8 +57,9 @@ fn main() {
 	for j in (0..IMG_HEIGHT).rev() {
 		eprintln!("Scanlines ramaining: {}", j);
 		for i in 0..IMG_WIDTH {
-			let u = (i as f64) / ((IMG_HEIGHT-1) as f64);
-			let v = (j as f64) / ((IMG_WIDTH-1) as f64);
+			let u = i as f64 / (IMG_WIDTH-1) as f64;
+			let v = j as f64 / (IMG_HEIGHT-1) as f64;
+			
 			let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
 			let pixel_color = ray_color(&r);
 			write_color(&mut out, pixel_color);
