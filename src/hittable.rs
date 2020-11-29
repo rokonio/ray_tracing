71e73ecccdc808f822a0cadbad03c47d04c71ae8
Rokonio <naelkanada@gmail.com>
2020-11-29 21:42:25 +0100

    Adding front-face tracking to hit_record and update the sphere class with normal determination

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,10 +1,18 @@
-use super::vec3::{Point3, Vec3};
+use super::vec3::*;
 use super::ray::Ray;
 
 pub struct HitRecord {
 	pub p: Point3,
 	pub normal: Vec3,
 	pub t: f64,
+	pub front_face: bool,
+}
+
+impl HitRecord {
+	pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+		self.front_face = dot(&r.direction(), &outward_normal) < 0.;
+		self.normal = if self.front_face {*outward_normal} else {-(*outward_normal)};
+	}
 }
 
 pub trait Hittable {
